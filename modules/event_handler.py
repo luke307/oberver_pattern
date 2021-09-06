aa8e92241f0f039b64c75ed80c3d315da0e0eb8b
luke307 <lukas.goebel96@gmx.de>
2021-09-06 20:16:31 +0200

    Pass Notify Func to Event_Handler

--- modules/event_handler.py
+++ modules/event_handler.py
@@ -1,6 +1,10 @@
 from watchdog.events import LoggingEventHandler
 
+from observer.subject import Subject
+
+
 class Event(LoggingEventHandler):
 
     def on_created(self, event):
-        pass
+        Subject.notify()
+        
